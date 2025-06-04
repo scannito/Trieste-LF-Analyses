9375c849ce0dad37ef73462c268c1db22dfd386b
Stefano Cannito <stefano.cannito@cern.ch>
2025-06-04 17:13:00 +0200

    Fixed LinkDef.h

--- AnalysisCore/LinkDef.h
+++ AnalysisCore/LinkDef.h
@@ -1,8 +1,8 @@
 #ifdef __CLING__
 
-4#pragma link off all globals;
-5#pragma link off all classes;
-6#pragma link off all functions;
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
 
 #pragma link C++ class LFInvMassFitter+;ù
 
