4be1cbf2ba416e139b58fb46bc1e8ca18f977f9a
Stefano Cannito <stefano.cannito@cern.ch>
2025-06-04 13:01:21 +0200

    Avoid default value in cpp file

--- LFInvMassFitter/src/Fitter.cpp
+++ LFInvMassFitter/src/Fitter.cpp
@@ -30,7 +30,7 @@ using namespace RooFit;
 
 ClassImp(Fitter);
 
-std::pair<Double_t, Double_t> Fitter::GetPhiPurityAndError(TH1F* h1PhiInvMass, std::string nameCanvas, Int_t isDataOrReco, Int_t isK0SOrPi, std::vector<Int_t> indices, bool printCanvas = false)
+std::pair<Double_t, Double_t> Fitter::GetPhiPurityAndError(TH1F* h1PhiInvMass, std::string nameCanvas, Int_t isDataOrReco, Int_t isK0SOrPi, std::vector<Int_t> indices, bool printCanvas)
 {
     h1PhiInvMass->SetTitle("; #it{M}(K^{+}K^{#minus}) (GeV/#it{c}^{2}); Counts");
     Double_t binsize = h1PhiInvMass->GetXaxis()->GetBinWidth(1);
@@ -122,9 +122,7 @@ std::pair<Double_t, Double_t> Fitter::GetPhiPurityAndError(TH1F* h1PhiInvMass, s
 }
 
 std::pair<Double_t, Double_t> Fitter::FitPhiK0S(TH2F* h2PhiK0SInvMass, std::vector<Int_t> indices, TFile* file,
-                                   const std::vector<Double_t>& params = {1., 1., 5., 5., 0.49, 0.003, -1.}, 
-                                   const std::vector<Double_t>& lowLimits = {1., 1., 1., 1., 0.48, 0.001, -1.7},
-                                   const std::vector<Double_t>& upLimits = {2., 2., 10., 10., 0.5, 0.01, 10})
+                                                const std::vector<Double_t>& params, const std::vector<Double_t>& lowLimits, const std::vector<Double_t>& upLimits)
 {
     // Definisci le variabili x e y
     RooRealVar x("x", "x", h2PhiK0SInvMass->GetXaxis()->GetXmin(), h2PhiK0SInvMass->GetXaxis()->GetXmax());
@@ -255,9 +253,7 @@ std::pair<Double_t, Double_t> Fitter::FitPhiK0S(TH2F* h2PhiK0SInvMass, std::vect
 }
 
 std::pair<Double_t, Double_t> Fitter::FitPhiPi(TH2F* h2PhiPiInvMass, std::vector<Int_t> indices, Int_t isTPCOrTOF, Int_t isDataOrMcReco, TFile* file,
-                                  const std::vector<Double_t>& params = {1., 1., 10., 10., 0., 1., 7., 1.}, 
-                                  const std::vector<Double_t>& lowLimits = {1., 1., 1., 1., -1., 0.001, 3., 0.001}, 
-                                  const std::vector<Double_t>& upLimits = {5., 5., 10., 10., 1.5, 2.5, 10., 5.})
+                                               const std::vector<Double_t>& params, const std::vector<Double_t>& lowLimits, const std::vector<Double_t>& upLimits)
 {
     // Definisci le variabili x e y
     RooRealVar x("x", "x", h2PhiPiInvMass->GetXaxis()->GetXmin(), h2PhiPiInvMass->GetXaxis()->GetXmax());
